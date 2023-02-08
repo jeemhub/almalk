83b32c21edaf2bd50eb82dec0daf3b7ffc94cc85
Youssef-Durgham <yusifdhrgamtrt@gmail.com>
2023-02-08 23:18:24 +0300

    edit on product slider

--- components/Productslider.js
+++ components/Productslider.js
@@ -1,12 +1,15 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Image from "next/future/image";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 
 function Productslider({ electroniccat, title }) {
-console.log(electroniccat, "fuck")
+  const router = useRouter()
+  const [isClient, setIsClient] = useState(false)
+
   const rowRef = useRef(null);
   const [isMoved, setIsMoved] = useState(false);
   const handleClick = (direction) => {
@@ -23,7 +26,33 @@ console.log(electroniccat, "fuck")
     }
   };
   const [activeTabIndex, setActiveTabIndex] = useState(0);
-    
+
+  function handleClicked(title,images,details,price,currency,location,isOwner,statuss,createdAt) {
+    router.push({
+      pathname: '/adsproduct',
+      query: {
+        title: title,
+        images: images,
+        details: details,
+        price: price,
+        currency:currency,
+        location:location,
+        isOwner:isOwner,
+        status:statuss,
+        createdAt:createdAt,
+
+
+
+      }
+    })
+  }
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null
+  }
   return (
     electroniccat.items.length >0 && <div className='relative flex bg-white w-[97%] mx-auto mt-3 tablet:mt-10 h-72 rounded-xl shadow-sm'>
     <div className='flex flex-row'>
@@ -40,10 +69,10 @@ console.log(electroniccat, "fuck")
             onClick={() => handleClick("left")}
           />
           {electroniccat.items.map((item, index) => (
-       <div key={item._id} className="w-48 h-56 tablet:w-60 tablet:h-64 relative flex flex-col">
-        <Link key={item._id} href={'/product/'+item._id}>
+       <div key={item._id} className="w-48 h-56 tablet:w-60 tablet:h-64 relative flex flex-col" onClick={()=> handleClicked(item.title, item.images,item.details,item.price, item.currency, item.location, item.isOwner, item.status, item.createdAt)}>
+        
        <Image className="snap-start bg-origin-padding border-none cursor-pointer w-40 h-40 tablet:w-48 tablet:h-48 object-fill pt-10 pb-2" src={item.images[0]} alt={item._id} width={200} height={200} key={item._id} ></Image>
-       </Link>
+       
        <div className='text-sm w-40'>{item.title}</div>
        </div>
  ))}
