3f9ff9420c19015e82a0b673d3099250147a12c3
Youssef-Durgham <yusifdhrgamtrt@gmail.com>
2023-02-09 23:46:49 +0300

    add categorys lists in left navigation slider

--- components/SideNavigation.js
+++ components/SideNavigation.js
@@ -1,4 +1,3 @@
-import React from "react";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import CloseIcon from "@material-ui/icons/Close";
 import {
@@ -7,6 +6,7 @@ import {
 import User_drop_down from './Dropdown';
 import { useRouter } from "next/router";
 import Link from 'next/link';
+import React, { useState, useEffect } from "react";
 
 export default function SideNavigation(props) {
     if (typeof window !== 'undefined') {
@@ -16,13 +16,19 @@ export default function SideNavigation(props) {
       }
       
       const router = useRouter()
+const [categories, setCategories] = useState([]);
+useEffect(() => {
+    fetch("http://ec2-52-23-248-118.compute-1.amazonaws.com:3000/categories")
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
 
   return (
     <div className={props.act ? "sideNavigation active":"sideNavigation"}>
       <div className="sideNavNameHead">
         <div className="pl-5 pt-12 text-white">
         <div className="text-xl font-bold">Browse</div>
-        <div className="block text-3xl -mt-2">Amazon</div>
+        <div className="block text-3xl -mt-2">Almalek</div>
         </div>
         <CloseIcon
           style={{
@@ -35,8 +41,8 @@ export default function SideNavigation(props) {
         />
       </div>
 
-      <div className="bg-[#fff] text-[#111] font-bold text-xl p-5" onClick={() => {router.push('/'); props.button}}>Amazon Home</div>
-      <div className="bg-[#fff] text-[#111] p-5 mt-2">
+      <div className="bg-[#fff] text-[#111] font-bold text-xl p-5" onClick={() => {router.push('/'); props.button}}>Almalek Home</div>
+      {/* <div className="bg-[#fff] text-[#111] p-5 mt-2">
         <div className="font-bold text-xl">Trending</div>
         <Link key={'1'} href={{
             pathname: '/categorys/[cat]',
@@ -44,30 +50,27 @@ export default function SideNavigation(props) {
           }}>
         <div className="mt-5 text-base cursor-pointer" onClick={props.button}>Mobiles</div>
         </Link>
-      </div>
+      </div> */}
       <div className="bg-[#fff] text-[#111] p-5 mt-2">
         <div className="font-bold text-xl">Departments</div>
-        <Link key={'2'} href={{
+        {categories.map((category) => (
+          <>
+          <div className="flex flex-row mt-2">
+          <Link key={category._id} href={{
             pathname: '/categorys/[cat]',
-            query: { cat: 'mobiles' },
-          }}>
-        <div className="mt-5 text-base cursor-pointer" onClick={props.button}>Mobiles</div>
+            query: { cat: `${category.name}` },
+          }} className="flex flex-row">
+        <div onClick={props.button} className="flex flex-row cursor-pointer"> 
+        <img src={category.image[0]} alt={category.name} className="w-10 h-10 object-contain bg-slate-200 rounded-lg" />
+        <div className="text-base cursor-pointer text-center ml-3 my-auto">{category.name}</div>
+        </div>
         </Link>
-        <Link key={'3'} href={'/categorys/tablets'}>
-        <div className="mt-5 text-base cursor-pointer" onClick={props.button}>Tablets</div>
-        </Link>
-        <Link key={'4'} href={{
-            pathname: '/categorys/[cat]',
-            query: { cat: 'jewelery' },
-          }}>
-        <div className="mt-5 text-base cursor-pointer" onClick={props.button}>jewelery</div>
-        </Link>
-        <Link key={'5'} href={'/categorys/Headphones'}>
-        <div className="mt-5 text-base cursor-pointer" onClick={props.button}>Headphones</div>
-        </Link>
-        <Link key={'5'} href={'/categorys/electronics'}>
-        <div className="mt-5 text-base cursor-pointer" onClick={props.button}>electronics</div>
-        </Link>
+          </div>
+        </>
+      ))}
+        
+        
+        
       </div>
     </div>
   );
---
+++ pages/categorys/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Categorys() {
+  return (
+    <div>Categorys</div>
+  )
+}
+
+export default Categorys
