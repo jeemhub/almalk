e0334bc8c28962feb2174260a9fe1508643c7a4e
Youssef Durgham <73782996+Youssef-Durgham@users.noreply.github.com>
2023-02-14 02:11:21 +0300

    Update [text].js
    update the ui

--- pages/search/[text].js
+++ pages/search/[text].js
@@ -1,87 +1,97 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
+import axios from 'axios';
 // eslint-disable-next-line import/no-anonymous-default-export, react/display-name
-export default ({ data }) => {
+export default () => {
   const router = useRouter();
 
-  function handleClicked(
-    title,
-    images,
-    details,
-    price,
-    currency,
-    location,
-    isOwner,
-    statuss,
-    createdAt
-  ) {
-    router.push({
-      pathname: "/adsproduct",
-      query: {
-        title: title,
-        images: images,
-        details: details,
-        price: price,
-        currency: currency,
-        location: location,
-        isOwner: isOwner,
-        status: statuss,
-        createdAt: createdAt,
-      },
-    });
-  }
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const id = router.query.text;
+
+useEffect(() => {
+  const fetchData = async () => {
+    setLoading(true);
+    if (id) {
+      const response = await axios.get(`http://almalk.org:3000/search/${id}/${page}`);
+      setItems(response.data);
+    }
+    setLoading(false);
+  };
+  fetchData();
+}, [page, id]);
+
+
+function handleClicked(title,images,details,price,currency,location,isOwner,statuss,createdAt) {
+  router.push({
+    pathname: '/adsproduct',
+    query: {
+      title: title,
+      images: images,
+      details: details,
+      price: price,
+      currency:currency,
+      location:location,
+      isOwner:isOwner,
+      status:statuss,
+      createdAt:createdAt,
+
+
+
+    }
+  })
+}
   return (
     <>
 
     <Header/>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 md:px-24 px-24">
-        {data.map((item) => {
-          if (item.images[0]) {
-            return (
-              <>
-                <div
-                  key={item._id}
-                  className="w-48 h-56 tablet:w-60 tablet:h-64 relative flex flex-col"
-                  onClick={() =>
-                    handleClicked(
-                      item.title,
-                      item.images,
-                      item.details,
-                      item.price,
-                      item.currency,
-                      item.location,
-                      item.isOwner,
-                      item.status,
-                      item.createdAt
-                    )
-                  }
-                >
-                  <img
-                    className="snap-start bg-origin-padding border-none cursor-pointer w-40 h-40 tablet:w-48 tablet:h-48 object-fill pt-10 pb-2"
-                    src={item.images[0]}
-                    alt={item._id}
-                    width={200}
-                    height={200}
-                    key={item._id}
-                  ></img>
-
-                  <div className="text-sm w-40">{item.title}</div>
-                </div>
-              </>
-            );
-          }
-        })}
+    {loading ? (
+      <div className="flex items-center justify-center h-64">
+        <div className="text-center text-2xl text-gray-500">Loading...</div>
       </div>
+    ) : (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-8">
+      {items.map(item => (
+        <div key={item._id} className="bg-white rounded-lg shadow-lg p-6" onClick={()=> handleClicked(item.title, item.images,item.details,item.price, item.currency, item.location, item.isOwner, item.status, item.createdAt)}>
+          <img className="h-48 w-full object-cover object-center" src={item.images[0]} alt={item.title} />
+          <h2 className="mt-2 text-lg font-medium leading-tight text-gray-800">{item.title}</h2>
+          <p className="mt-2 text-sm font-medium text-gray-700 truncate">{item.details}</p>
+          <p className="mt-2 text-sm font-medium text-indigo-500">{item.price} {item.currency}</p>
+        </div>
+      ))}
+     
+    </div>
+    )}
+    <div className='flex mt-8 mb-10'>
+      <div className="flex space-x-3 mx-auto">
+        <button
+          onClick={() => page > 1 && setPage(page - 1)}
+          className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded"
+          disabled={page === 1}
+        >
+          Prev Page
+        </button>
+        <button
+          onClick={() => items.length === 20 && setPage(page + 1)}
+          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Next Page
+        </button>
+      </div>
+    </div>
     </>
   );
 };
-export const getServerSideProps = async (context) => {
-  const text = context.query.text;
-  const res = await fetch(`http://almalk.org:3000/search/${text}`);
-  const data = await res.json();
-  return {
-    props: {
-      data,
-    },
-  };
-};
+// export const getServerSideProps = async (context) => {
+//   const text = context.query.text;
+//   const res = await fetch(`http://almalk.org:3000/search/${text}`);
+//   const data = await res.json();
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
