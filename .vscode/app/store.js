597ea9178e1ea4e6fde7bfa470302011bc5f9181
Youssef Durgham <73782996+Youssef-Durgham@users.noreply.github.com>
2023-02-28 00:03:41 +0300

    fix it

--- .vscode/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import cartSlice from "../slice/cartSlice"
-
-
-export const store = configureStore({
-    reducer:{
-        cart: cartSlice,
-    },
-})
